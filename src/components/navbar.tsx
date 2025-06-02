00783031eb7aac8ed150777a4d19f5a0eef72d87
carlosAndress101 <carlos.hinestrozap@unac.edu.co>
2025-06-02 10:59:14 -0500

    feat(ui): update button gradient and navbar dynamic titles
    - Replace solid primary button color with gradient for better visual appeal
    - Make navbar titles dynamic based on current route to improve user context

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,14 +1,34 @@
 import UserButton from "@/features/auth/components/userButton";
 import MobileSidebar from "./mobile-sidebar";
+import { usePathname } from "next/navigation";
+
+const pathnameMap = {
+	tasks: {
+	  title: "Mis tareas",
+	  description: "Ver todas tus tareas aquí",
+	},
+	projects: {
+	  title: "Mis proyectos",
+	  description: "Ver todos tus proyectos aquí",
+		},
+  };
+
+const defaultMap = {
+	title: "Karma Kanban",
+	description: "Organiza tus proyectos y tareas aquí",
+};
 
 const Navbar = () => {
+	const pathname = usePathname();
+	const pathnameParts = pathname.split("/");
+	const pathnameKey = pathnameParts[3] as keyof typeof pathnameMap;
+
+	const { title, description } = pathnameMap[pathnameKey] || defaultMap;
 	return (
 		<nav className="pt-4 px-6 flex items-center justify-between">
 			<div className="flex-col hidden lg:flex">
-				<h1 className="text-2xl font-semibold">Home</h1>
-				<p className="text-muted-foreground">
-					Monitor all your projects and tasks
-				</p>
+				<h1 className="text-2xl font-semibold">{title}</h1>
+				<p className="text-muted-foreground">{description}</p>
 			</div>
 			<MobileSidebar />
 			<UserButton />
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -10,7 +10,7 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "bg-primary text-primary-foreground shadow-xs hover:bg-primary/90",
+          "bg-gradient-to-b from-blue-600 to-blue-700 text-primary-foreground hover:from-blue-700 hover:to-blue-700",
         destructive:
           "bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
