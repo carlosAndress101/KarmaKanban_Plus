005a61440c0634f19011922a1cc6af84e8302cbf
SamuelAroca <samuel.arocad@unac.edu.co>
2025-06-07 02:20:47 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  basePath: "/KarmaKanban",
-  trailingSlash: true, // opcional, pero ayuda con rutas internas y estáticos
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
