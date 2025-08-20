239153d3631f641f740face47984b36ff702c024
SamuelAroca <samuel.arocad@unac.edu.co>
2025-08-19 19:12:57 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // basePath: "/KarmaKanban", // Commented out for local development
+  basePath: "/KarmaKanban", // Commented out for local development
 };
 
 export default nextConfig;
